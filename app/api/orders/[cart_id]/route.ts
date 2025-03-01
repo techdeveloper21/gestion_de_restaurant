4a9a1aca9184fed1722f9795edfa8cf5f602f6a3
techdeveloper21 <techdeveloper2104@gmail.com>
2025-03-01 22:39:49 +0100

    fixing errors with deply change type  v-4.15

--- app/api/orders/[cart_id]/route.ts
+++ app/api/orders/[cart_id]/route.ts
@@ -1,11 +1,11 @@
 import { getCartItemsByCartId } from "@/services/cartService";
 import { getProductBySlug } from "@/services/productService";
 
-export async function GET(req: Request, { params }: { params: { cart_id: number } }) { // ✅ Use 'string' instead of 'any'
+export async function GET(req: Request, { params }: { params: { cart_id: string } }) { // ✅ Use 'string' instead of 'any'
     console.log("Fetching orders products details:", params.cart_id);
 
     /// Get cart items [{ cart_id: , product_slug: '', quantity:  }]
-    const cartItems = await getCartItemsByCartId(params.cart_id); 
+    const cartItems = await getCartItemsByCartId(parseInt(params.cart_id, 10)); 
 
     const result = [];
 
