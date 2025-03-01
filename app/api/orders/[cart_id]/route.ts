c6cc99453d6a7e0eb37febe6fbc5d33dc5dfd325
techdeveloper21 <techdeveloper2104@gmail.com>
2025-03-01 22:46:12 +0100

    fixing errors adding cotext in Get orderv-4.17

--- app/api/orders/[cart_id]/route.ts
+++ app/api/orders/[cart_id]/route.ts
@@ -1,8 +1,9 @@
+import { NextRequest } from "next/server";
 import { getCartItemsByCartId } from "@/services/cartService";
 import { getProductBySlug } from "@/services/productService";
 
-export async function GET(req: Request, context: { params: { cart_id: string } }) {  
-    const cartId = parseInt(context.params.cart_id, 10); // Convert string to number
+export async function GET(req: NextRequest, { params }: { params: { cart_id: string } }) {  
+    const cartId = parseInt(params.cart_id, 10); // Convert string to number
     if (isNaN(cartId)) return Response.json({ error: "Invalid cart ID" }, { status: 400 });
 
     console.log("Fetching orders products details:", cartId);
